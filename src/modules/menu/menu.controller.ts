79bad55aa3c0c544d49f2d9d1b53309f31da659d
Mikyas~ <mikyasnegash2@gmail.com>
2026-07-29 21:53:15 +0300

    added explicit Promise<void> return types

--- src/modules/menu/menu.controller.ts
+++ src/modules/menu/menu.controller.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from "express";
-import {createCategoryService,getCategoriesService,updateCategoryService,deleteCategoryService} from "./menu.service.js"
+import {
+  createCategoryService,
+  getCategoriesService,
+  updateCategoryService,
+  deleteCategoryService
+} from "./menu.service.js";
 
 export async function createCategory(
   req: Request,
   res: Response
-) {
+): Promise<void> {
   try {
 
     const { name } = req.body;
@@ -38,15 +43,16 @@ export async function createCategory(
 export async function getCategories(
   req: Request,
   res: Response
-) {
- try{
+): Promise<void> {
+  try {
     const userId = req.user.userId;
-    const categories = await getCategoriesService(userId)
+    const categories = await getCategoriesService(userId);
+
     res.status(200).json({
       message: "Categories fetched successfully",
       categories
     });
- }catch(error) {
+  } catch (error) {
     console.error(error);
     res.status(500).json({
       message:
@@ -54,36 +60,39 @@ export async function getCategories(
           ? error.message
           : "Something went wrong"
     });
- }
+  }
 }
 
-export async function updateCategory(req: Request, res: Response) {
-    try{
+export async function updateCategory(
+  req: Request,
+  res: Response
+): Promise<void> {
+  try {
     const { id } = req.params;
     const { name } = req.body;
     const userId = req.user.userId;
 
     if (!id) {
-  return res.status(400).json({
-    message: "Category id is required"
-  });
-}
+      return res.status(400).json({
+        message: "Category id is required"
+      });
+    }
 
     const category = await updateCategoryService(id, name, userId);
+
     res.status(200).json({
-  message: "Category updated successfully",
-  category
-});
-    }catch(error){
-         console.error(error);
+      message: "Category updated successfully",
+      category
+    });
+  } catch (error) {
+    console.error(error);
     res.status(500).json({
       message:
         error instanceof Error
           ? error.message
           : "Something went wrong"
     });
- }
-    
+  }
 }
 export async function deleteCategory(
   req: Request,
