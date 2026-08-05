d573269ba42da4b2333a36291da33ddd032c81b6
Mikyas~ <mikyasnegash2@gmail.com>
2026-08-05 17:37:02 +0300

    deleting all unnessary files ai created

--- src/modules/ingredient/ingredient.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-export declare function createIngredient(req: Request, res: Response): Promise<Response | void>;
-export declare function getIngredients(req: Request, res: Response): Promise<Response | void>;
-export declare function updateIngredient(req: Request, res: Response): Promise<Response | void>;
-export declare function deleteIngredient(req: Request, res: Response): Promise<Response | void>;
-//# sourceMappingURL=ingredient.controller.d.ts.map
--- src/modules/restaurants/restaurant.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-export declare function create(req: Request, res: Response): Promise<void>;
-export declare function getMyRestaurant(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
-export declare function getAllRestaurants(req: Request, res: Response): Promise<void>;
-export declare function updateMyRestaurant(req: Request, res: Response): Promise<void>;
-export declare function deleteMyRestaurant(req: Request, res: Response): Promise<void>;
-//# sourceMappingURL=restaurant.controller.d.ts.map
