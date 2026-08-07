841730a1b393cf2123cf58ba05afe12dad2f739c
Mikyas~ <mikyasnegash2@gmail.com>
2026-08-07 21:01:06 +0300

    deleting all unnessary files cloud add

--- src/utils/jwt.d.ts
+++
@@ -1,7 +0,0 @@
-export interface AuthPayload {
-    userId: string;
-    role: string;
-}
-export declare function generateToken(payload: AuthPayload): string;
-export declare function verifyToken(token: string): AuthPayload;
-//# sourceMappingURL=jwt.d.ts.map
